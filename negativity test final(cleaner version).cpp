8a2e91bf54622e77f666d9a75391e5a976346db6
Progenitor-Aaditya <140176788+Progenitor-Aaditya@users.noreply.github.com>
2023-12-31 17:40:11 +0530

    Add files via upload
    here we use if - else if - else  statements

---
+++ negativity test final(cleaner version).cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a ;
+    cout<<"enter value of A "<<endl;
+    cin>>a;
+ 
+    if(a>0)
+    {
+        cout<<"A is a Positive number"<<endl;
+    }
+    else if(a<0)
+    {
+        cout<<"A is a Negatvie number"<<endl;
+    }
+    else{
+        cout<<"A is zero"<<endl;
+    }
+    
+}
