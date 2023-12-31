47cdcb3c9885dd28de9dc1d8b838ce20beb95f9b
Progenitor-Aaditya <140176788+Progenitor-Aaditya@users.noreply.github.com>
2023-12-31 15:00:15 +0530

    Add files via upload
    this code we check if a number is negative or positive using if else

---
+++ negativity test.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    //cout<<"value of n is :"<<n<<endl;
+    //if n is positive 
+    if(n>0)
+   {
+       cout<<"n is a positive number"<<endl;
+       
+    }
+    else{
+       cout<<"n is a negative number"<<endl;
+    }
+}
