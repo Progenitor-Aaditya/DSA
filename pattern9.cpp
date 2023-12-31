f123f23da4cf1e4c49e51cb44a588e7108d08bd4
Progenitor-Aaditya <140176788+Progenitor-Aaditya@users.noreply.github.com>
2023-12-31 22:30:33 +0530

    Add files via upload
    prints pattern for n=4 | 1-->23--->345-->4567

---
+++ pattern9.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    int i,j,k;
+    i=1;
+    k=1;
+    while(i<=n)
+    {
+        j=1;
+        while(j<=i)
+        {
+            cout<<k;
+            k++;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+}
