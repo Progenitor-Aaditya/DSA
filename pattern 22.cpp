b11796ce1c356afc3e28c1123cf23d24270a0cf9
Progenitor-Aaditya <140176788+Progenitor-Aaditya@users.noreply.github.com>
2024-01-02 15:53:18 +0530

    Add files via upload
    ****
     ***
      **
       *

---
+++ pattern 22.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cin>>n;
+    i=n;
+    while(i>=1)
+    {
+    
+    int space =n-i;
+    while(space)
+    {
+        cout<<" ";
+        space--;
+    }
+    j=1;
+    while(j<=i)
+    {
+        cout<<"*";
+        j++;
+    }
+    cout<<endl;
+    i--;
+    }
+}
